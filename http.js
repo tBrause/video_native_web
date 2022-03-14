2a455f9f5bfb5235190f231c283eaa341209c8ef
tBrause <brause.torsten@gmail.com>
2022-03-14 23:08:29 +0100

    commonjs und ECMA Server

--- http.js
+++ http.js
@@ -1,15 +1,42 @@
 /**
  *
- * Modul http laden
+ * http-Modul laden
  *
  */
 const http = require("http");
 
 /**
  *
- * Einen Server erstellen
+ * Einen Server erstellen mit commonjs
  *
  */
 const server = http.createServer(function (req, res) {
-  // ...
+  // response
+  res.write(`commonjs`);
+
+  // response beenden (sonst endlos)
+  res.end();
+});
+
+// Server starten und Callback
+server.listen(`3000`, () => {
+  console.log(`commonjs`);
+});
+
+/**
+ *
+ * Einen Server erstellen mit ECMA-Script
+ *
+ */
+const serverECMA = http.createServer((req, res) => {
+  // response
+  res.write(`ECMA-Script`);
+
+  // response beenden (sonst endlos)
+  res.end();
+});
+
+// Server starten und Callback
+serverECMA.listen(`3001`, () => {
+  console.log(`ECMA-Script`);
 });

36a05aee40e959e96af0c18019988419f89b42ca
tBrause <brause.torsten@gmail.com>
2022-03-15 00:50:34 +0100

    Export Import Module

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(`hello world`);
+/**
+ *
+ * Module laden
+ *
+ */
+// API Modul
+const http = require("http");
+
+// Eigenes Modul
+const handle = require("./server/handle");
+
+/**
+ *
+ * Server erstellen
+ *
+ */
+const server = http.createServer(handle);
+
+/**
+ *
+ * Server starten und Callback
+ *
+ */
+server.listen("3002", () => {
+  console.log(`Server on 3002`);
+});
